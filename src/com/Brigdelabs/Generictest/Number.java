32c3fe8412eb8ae5bc08e7750b2666efdae2d381
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-20 23:43:38 +0530

    FOund maximum among 3 floats

--- src/com/Brigdelabs/Generictest/Number.java
+++ src/com/Brigdelabs/Generictest/Number.java
@@ -11,13 +11,30 @@ public class Number {
 				System.out.println(" c is greater");
 			}
 	 }
+	public void compare(Float a ,Float  b ,Float  c )
+	 {
+		 if (a.compareTo(b) > -1 && a.compareTo(c) > -1) {
+				System.out.println("a is greater");
+			} else if ((b.compareTo(a)) > -1 && (b.compareTo(c)) > -1) {
+				System.out.println(" b is greater");
+			} else if ((c.compareTo(a)) > -1 && (c.compareTo(b)) > -1) {
+				System.out.println(" c is greater");
+			}
+	 }
 	
 public static void main(String[] args) {
 	Integer a = 10;
 	Integer b = 20;
 	Integer c = 30;
-	Number obj= new Number();
-	obj.compare(a, b, c);
+	Number integerObj= new Number();
+	integerObj.compare(a, b, c);
+	
+	Float d=1.2f;
+	Float e=3.2f;
+	Float f=0.2f;
+	Number floatObj= new Number();
+	floatObj.compare(d, e, f);
+	
 	
 	
 	
