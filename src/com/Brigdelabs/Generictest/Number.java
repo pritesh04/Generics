373a3700af59a25430f551b023777068c9e9a10d
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-20 23:40:35 +0530

    FOund maximum among 3 integers

--- src/com/Brigdelabs/Generictest/Number.java
+++ src/com/Brigdelabs/Generictest/Number.java
@@ -1,7 +1,26 @@
 package com.Brigdelabs.Generictest;
 
 public class Number {
+	public void compare(Integer a ,Integer  b ,Integer  c )
+	 {
+		 if (a.compareTo(b) > -1 && a.compareTo(c) > -1) {
+				System.out.println("a is greater");
+			} else if ((b.compareTo(a)) > -1 && (b.compareTo(c)) > -1) {
+				System.out.println(" b is greater");
+			} else if ((c.compareTo(a)) > -1 && (c.compareTo(b)) > -1) {
+				System.out.println(" c is greater");
+			}
+	 }
+	
 public static void main(String[] args) {
+	Integer a = 10;
+	Integer b = 20;
+	Integer c = 30;
+	Number obj= new Number();
+	obj.compare(a, b, c);
+	
+	
+	
 	
 }
 }
