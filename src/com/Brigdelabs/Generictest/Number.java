38a53eda129e83ed9b2084e05db98ed4e34be5b0
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-23 17:58:51 +0530

    Done Generic Class

--- src/com/Brigdelabs/Generictest/Number.java
+++ src/com/Brigdelabs/Generictest/Number.java
@@ -1,6 +1,24 @@
 package com.Brigdelabs.Generictest;
 
-public class Number {
+import com.Brigdelabs.Generictest.Number;
+
+public class Number<T extends Comparable<T>> {
+	
+	T obj1; T obj2; T obj3;
+	
+	
+
+	public Number(T obj1, T obj2, T obj3) {
+		
+		this.obj1 = obj1;
+		this.obj2 = obj2;
+		this.obj3 = obj3;
+	}
+	
+	public void compute()
+	{
+		this.compare(obj1, obj2, obj3);
+	}
 
 	public static <T extends Comparable<T>> void compare(T a, T b, T c) {
 		if (a.compareTo(b) > -1 && a.compareTo(c) > -1) {
@@ -16,18 +34,29 @@ public class Number {
 		Integer a = 10;
 		Integer b = 20;
 		Integer c = 30;
+		//Number integerObj = new Number();
 		compare(a, b, c);
 
 		Float d = 1.2f;
 		Float e = 3.2f;
 		Float f = 0.2f;
-		compare(d, e, f);
-		
+		//Number floatObj = new Number();
+		//compare(d, e, f);
+
 		String g = "pritesh";
 		String h = "aniket";
 		String i = "yash";
-		compare(g, h, i);
-
+		//Number stringObj = new Number();
+		//compare(g, h, i);
+		
+		Number inta = new Number(a, b, c);
+		Number floata = new Number(d,e,f);
+		Number stringa = new Number(g,h,i);
+		
+		inta.compute();
+		floata.compute();
+		stringa.compute();
+		
 
 	}
 }
